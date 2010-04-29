54891fda3f188dc7bde114c903210772c2418cf6
innovia <ami.mahloof@gmail.com>
2010-04-28 23:29:55 -0400

    resched phase 3 - custom repeat

--- public/javascripts/event_templates.js
+++ public/javascripts/event_templates.js
@@ -45,49 +45,77 @@ function repeat_options(){
 					break;
 
 					case "Daily":
-						trigger_repeat();
+						show_repeat_options();
 						$('#start_clone').val($('#start_date').val());
 						$('#range').show();
+						
 						$('#end_repeat_radio_on_date').bind('click', function() {
+							$('#after').html('');
 							$('#on_date').html('<input type="text" size="10" name="event_template[until]" id="event_template_until" class="date_picker">');
-							$('.date_picker').datepicker({
+							
+							$('#event_template_until').datepicker({
 							    beforeShow: function(input, inst)
 							    {
 							        inst.dpDiv.css({marginTop: -input.offsetHeight + 'px', marginLeft: input.offsetWidth + 'px'});
-							    }
+							    },
+									onSelect: function(dateText, inst){
+										update_repeat_indicator_with_until();
+									}
 							});
+							
+							// Add 30 days to until field automatically
+							$('#event_template_until').val(Date.parse($('#start_date').val()).add(1).month().toString('MM/dd/yyyy'));
+						 	
 							$('#event_template_until').focus();
 							
-						 	var d = 	$.datepicker.parseDate('mm/dd/yy', $('#start_date').val());
-							 	  d.setDate(d.getDate() + 30); // Add 30 days
-						      $('#event_template_until').datepicker('setDate', d);
-						  $('#repeat_indicator').html('Every ' + $('#event_template_interval :selected').val() + ' ' + $('#repeat_every_x').html() + ', Until ' + $.datepicker.parseDate('M d, YY', $('#event_template_until').val()))
-							$('#after').html('');
+							update_repeat_indicator_with_until();
+						
 						});
+						
 						$('#end_repeat_radio_after').bind('click', function() {
 							$('#on_date').html('');
 							$('#after').html('<input type="text" size="2" name="event_template[count]" id="event_template_count"> Occurrences');
+							$('#repeat_indicator').html('Every ' + $('#event_template_interval :selected').val() + ' ' + $('#repeat_every_x').html() );
+					
+							$('#event_template_count').bind('keyup', function() {
+									$('#repeat_indicator').html('Every ' + $('#event_template_interval :selected').val() + ' ' + $('#repeat_every_x').html()
+										+ '<br/>End after ' + $('#event_template_count').val() + ' times (occurrences / classes)');
+							});
 						});
+						
+										
 					break;
 					
 					default: // Every X selected
-						trigger_repeat();
+						show_repeat_options();
 					break;
 				} // end of switch
 	 }); // end of change function
 } // end of repeat_options
 
-function trigger_repeat() {
+function show_repeat_options() {
 	$('#repeat_indicator').html($('#event_template_repeat').val()).show();
 	$('#repeat_every_x').html($('#event_template_repeat :selected').text().replace('Every', '').trim());
 	$('#custom_repeat').hide();
 	$('#interval_select').show();
 };
 
+function update_repeat_indicator_with_until(){
+	$('#repeat_indicator').html('Every ' + $('#event_template_interval :selected').val() + ' ' + $('#repeat_every_x').html() + 
+															', Until ' + Date.parse($('#event_template_until').val()).toString('dddd, MMMM d, yyyy')
+															);
+															
+}
 
 function end_repeat_options(){
 	
 	$('#event_template_interval').bind('change', function() {
+		$('#after').html('');
+		$('#end_repeat_radio_after').attr('checked', false);
+		
+		$('#on_date').html('');
+		$('#end_repeat_radio_on_date').attr('checked', false);
+		
 		var val = $('#repeat_every_x'); 
 		
 		if ($('#event_template_interval :selected').val() > 1) {
